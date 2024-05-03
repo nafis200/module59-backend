037580823b19581360446b7ae8e8663bb364a71a
Nafis Ahamed <nafisahamed14@gmail.com>
2024-05-03 15:48:34 +0600

    jewt token

---
+++ jwttoken.js
@@ -0,0 +1,17 @@
+// # > require('crypto').randomBytes(64)
+// # > require('crypto').randomBytes(64).toString('h 
+// # > require('crypto').randomBytes(64).toString('hex')
+
+
+
+// DB_USER = nafisiqbal
+// DB_PASS = wrnuBF7lVlnFzlAK
+// ACCESS_TOKEN_SECRET = 26b5f472c3a1ae527fdbe0fc7cd179abaab9b76bdbfcc7b1c96f248d1816725114875ba6230f4f2812c1adbdf6b86022d4ee14dd17be4c46b0b246e42c303d43
+
+// jwt.sign(payload,secret,{expiresIn:})
+
+// how to store token in the client side
+
+// 1. memory
+// 2.local storage 
+// 3.cookies: http only
